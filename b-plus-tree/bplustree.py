81e37810dec33075f18a2a9cbc4c14e6eee6dd1b
Clayton Stangeland <clayton.stangeland@gmail.com>
2013-12-30 17:39:09 -0600

    working b+ tree

--- b-plus-tree/bplustree.py
+++ b-plus-tree/bplustree.py
@@ -20,7 +20,10 @@ def same_type_check(a, b):
                 raise Exception("left is " + a.__class__.__name__ + " while right is " +
                                 b.__class__.__name__)
 
-class Node:
+def iter_len(a):
+        return sum(1 for _ in a)
+
+class Node(object):
         def __init__(self):
                 self.keys = []
                 self.values = []
@@ -46,43 +49,34 @@ class Node:
 
         def take_left(self):
                 half = max((self.count - MinNodeSize)/2, 1)
-                items = []
-#                print "h=",half, " k=",len(self.keys), " c=",self.count
-                for i in xrange(half):
-                        items.append((self.keys[i], self.values[i]))
-                c = self.count
-                self.keys = self.keys[c - half:]
-                self.values= self.values[c-half:]
-                return items
+                keys = self.keys[:half]
+                values = self.values[:half]
+                self.keys = self.keys[half:]
+                self.values= self.values[half:]
+                return (keys, values)
 
         def take_right(self):
                 half = max((self.count - MinNodeSize)/2, 1)
-                items = []
-                for i in xrange(half):
-                        x = self.count - half + i
-                        items.append((self.keys[x], self.values[x]))
-                c = self.count
-                self.keys = self.keys[:half]
-                self.values = self.values[:half]
-                return items
+                keys = self.keys[-half:]
+                values = self.values[-half:]
+                self.keys = self.keys[:-half]
+                self.values = self.values[:-half]
+                return (keys, values)
 
         def add_from_right(self, node):
-                self.add_range(node.take_right())
+                self.add_left(*node.take_right())
 
         def add_from_left(self,node):
-                self.add_range(node.take_left())
+                self.add_right(*node.take_left())
 
-        def add_range(self, items):
-                for k,v in items:
-                        self.add(k,v)
+        def add_left(self, keys, values):
+                self.keys = keys + self.keys
+                self.values = values + self.values
+        def add_right(self, keys, values):
+                self.keys += keys
+                self.values += values
 
         def add(self, key, value):
-#                if self.is_internal:
-#                        type_check(value, Node)
-                
-#                for x in self.values:
-#                        same_type_check(x, value)
-
                 i = binary_search(self.keys, key)                                
                 if i >= 0:
                         self.values[i] = value
@@ -109,9 +103,8 @@ class Node:
 class Leaf(Node):
         def __init__(self):
                 Node.__init__(self)
-        def new(self):         return Leaf()
-
-
+        def new(self):         
+                return Leaf()
         def remove(self, key):
                 i = binary_search(self.keys, key)
                 if i < 0:
@@ -124,8 +117,8 @@ class Leaf(Node):
 class Internal(Node):
         def __init__(self): 
                 Node.__init__(self)                
-        def new(self):         return Internal()
-        
+        def new(self):         
+                return Internal()        
         def get_node(self, key):
                 i = binary_search(self.keys, key)
                 if i < 0:
@@ -134,7 +127,6 @@ class Internal(Node):
                 return (i, self.values[i])
 
         def update(self, index, node):
-#                print "i=",index, " k=",len(self.keys), " n=", len(node.keys)
                 self.keys[index] = node.keys[0]
 
         def update_node(self, index, key):
@@ -231,49 +223,66 @@ class BTree:
         def _remove_root_internal(self, key):
                 nodes = self.move_to(key)
                 leaf = nodes[-1][1]
-                if not leaf.remove(key): return
-                if not self._rebalance(nodes):
-                        return 
+                if not leaf.remove(key): 
+                        return
+                if not leaf.is_empty:
+                        self._update_parents(nodes)
+                #if not self._rebalance(nodes):
+                #        return 
                 self.root = nodes[0][1]
 
+        def _update_parents(self, nodes):
+                for x in xrange(len(nodes)-1):
+                        index, parent =nodes[-(x+2)]
+                        _, child = nodes[-(x + 1)]
+                        parent.update(index, child)
+                        
+
         def _rebalance(self, nodes):
-                leaf = nodes[-1][1]
-                if leaf.count >= MinNodeSize: return
+                if len(nodes) < 2: 
+                        return
+
+                left  = None
+                right = None
+                leaf  = nodes[-1][1]
+                if leaf.count >= MinNodeSize: 
+                        return
                 
                 left_nodes = self._left_child(nodes)
-                left = left_nodes[-1][1]
-                if left and left.count > MinNodeSize:
-                        same_type_check(left, leaf)
+                if left_nodes:
+                        left = left_nodes[-1][1]
+                        if left and left.count > MinNodeSize:
+                                same_type_check(left, leaf)
 
-                        leaf.add_from_right(left)
-                        child_index, parent = left_nodes[-2]
-                        parent.update(child_index, leaf)
-                        return
+                                leaf.add_from_right(left)
+                                self._update_parents(nodes)
+                                return
                 right_nodes = self._right_child(nodes)
-                right = right_nodes[-1][1]
-                if right and right.count > MinNodeSize:
-                        same_type_check(right, leaf)
+                if right_nodes:
+                        right = right_nodes[-1][1]
+                        if right and right.count > MinNodeSize:
+                                same_type_check(right, leaf)
 
-                        leaf.add_from_left(right)
-                        child_index, parent = right_nodes[-2]
-                        parent.update(child_index+1, right)
-                        return
+                                leaf.add_from_left(right)
+                                self._update_parents(right_nodes)
+                                return
                 
                 if left:
+#                        print "in left"
                         same_type_check(left, leaf)
 
-                        left.add_range(leaf.items())
-                        child_index, parent = nodes[-2]
-                        parent.update(child_index-1, left)
+                        left.add_right(leaf.keys, leaf.values)
+                        child_index, parent = nodes[-2]    
                         parent.remove_at(child_index)
                         self._rebalance(nodes[:-1])
                         return
                 if right:
+ #                       print "in right"
                         same_type_check(right, leaf)
 
-                        right.add_range(leaf.items())
-                        child_index, parent = nodes[-2]
-                        parent.update(child_index+1, right)
+                        right.add_left(leaf.keys, leaf.values)
+                        self._update_parents(right_nodes)
+                        child_index, parent = nodes[-2]                        
                         parent.remove_at(child_index)
                         self._rebalance(nodes[:-1])
                         return
@@ -304,12 +313,13 @@ class BTree:
                         i-= 1
                 if i < 0: return None
                 left_path = nodes[:i]
-                while True:
+                while len(left_path) < len(nodes):
                         left_path.append((index2, parent))
                         if parent.is_leaf: break
                         parent = parent.values[index2]
                         index2 = parent.count -1 # right most
                 return left_path
+
         def _right_child(self, nodes):
                 if len(nodes) < 2: return None
                 index, onde = nodes[-1]
@@ -321,7 +331,7 @@ class BTree:
                         i -= 1
                 if i < 0: return None
                 right_path = nodes[:i]
-                while True:
+                while len(right_path) < len(nodes):
                         right_path.append((index2, parent))
                         if parent.is_leaf: break
                         parent = parent.values[index2]
@@ -341,16 +351,14 @@ class BTree:
                         nodes.append((next_index, node))
                         self._move_to(nodes, key, next_node)
                 else:                
-                        i = binary_search(node.keys, key)
-                        #if i < 0: i = ~i
+                        i = binary_search(node.keys, key)                        
                         nodes.append((i, node))
 
         def items(self):
                 if self.root:
                         for x in self._items(self.root):
                                 yield x
-                else:
-                        return
+                
         def _items(self, node):
                 if node.is_leaf:
                         for x in node.items():
@@ -362,32 +370,66 @@ class BTree:
 
 
 import unittest
-from random import random
+from random import random, seed, randint
+#import cProfile
+#import pstats
+
 
 class Test(unittest.TestCase):
+        def _assert_order(self, x):
+                i = None
+                for x2 in x:
+                        if i:
+                                self.assertGreater(x2, i)
+                        i = x2
+
+        # def setUp(self):
+        #         self.pr = cProfile.Profile()
+        #         self.pr.enable()
+
+        # def tearDown(self):
+        #         p = pstats.Stats(self.pr)
+        #         p.strip_dirs()
+        #         p.sort_stats('cumtime')
+        #         p.print_stats()
+
         def test(self):
-                count = 100000
-                items = list(set(random() for x in xrange(count*2)))[:count]
+                seed_value = randint(0, sys.maxint)
+                print "testing with random.seed=", seed_value
+                seed(seed_value)
+
+                count = 1000000
+                items = list(set(random() for x in xrange(count*2)))[:count] # unique random values
                 
                 tree = BTree()
                 for x in items:
                         tree.add(x, x)
 
-                values = [k for k,v in tree.items()]
-                sorted = list(items)
-                sorted.sort()
-                
-                for i in xrange(len(values)):
-                        self.assertEqual(sorted[i], values[i])
-                
+                self._assert_order(tree.items())
+                        
                 for item in items:
                         x = tree.get(item)
                         self.assertEqual(item, x)
-                        
+                                                
+                for item in items:
+                        #print count
+                        #self._assert_order(tree.items())
+                        #if count <= 90082:
+                        #self.assertEqual(count, iter_len(tree.items()))
+                        tree.remove(item)
+                        count -= 1
+
+                for x in items:
+                        tree.add(x, x)
+
+                count = len(items)
+                items.sort()
                 for item in items:
                         tree.remove(item)
+                        count -= 1
+                
 
-                self.assertEqual(0, len(list(tree.items())))
+                self.assertEqual(0, iter_len(tree.items()))
 
-if __name__=="__main__":
+if __name__=="__main__":                
         unittest.main()
